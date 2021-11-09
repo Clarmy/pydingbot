5629217bd5337351a210a4fbbdc6ec714c2d4eb7
wentao.li <wentao.li@moji.com>
2021-11-10 00:51:05 +0800

    chore: 更新文档，更新版本号

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(requirements_path) as f:
 
 setuptools.setup(
     name='pydingbot',
-    version='0.0.1',
+    version='0.0.2',
     author='Wentao Li',
     author_email='clarmylee92510@gmail.com',
     description='A package to make dingbot easily to use',
